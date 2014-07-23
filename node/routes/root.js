5bff024f5a23344efde94a00d0fbf52bd980327a
Hoolean <me@hoolean.com>
2014-07-23 16:27:21 +0100

    Setup server and basic routes

---
+++ node/routes/root.js
@@ -0,0 +1,13 @@
+function registerMiddleware (app)
+{
+	app.get('/', handleRoot);
+}
+
+module.exports = exports = registerMiddleware;
+
+var PUBLIC_ROOT = __dirname + '/public';
+
+function handleRoot (request, response)
+{
+	response.sendfile('index.html', { root: PUBLIC_ROOT });
+}
